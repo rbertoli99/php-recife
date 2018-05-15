fa309f62d0094b85be24f98b1996e1694f706492
Rodrigo Bertoli Rocha <30940156+rbertoli99@users.noreply.github.com>
2018-05-15 16:15:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Openshift Workshop v3.1</h1> ";
+echo "<h1>Openshift Workshop v4.0 com Jenkins Pipeline</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
